c5320ac7003dd339c556d0deaffb26120b4547ca
你永远可以相信皮卡丘 <2569217433@qq.com>
2021-01-23 21:50:51 +0800

    乘法口诀和二分查找

---
+++ 乘法口诀表.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+// 乘法口诀表
+/*{
+int i = 0;
+int j = 0;
+int o = 0;
+for (i = 0; i <= 9; i++)
+{
+for (j = 1; j <= i; j++)
+{
+o = i * j;
+printf("%d*%d=%d ",j, i, o);
+}
+printf("\n");
+}
+}*/
+
+
+//打印3的倍数
+/*{
+int i = 0;
+for (i = 3; i <= 100; i++)
+{
+if (i % 3 == 0)
+printf("%d\n", i);
+}
+
+
+return 0;
+}*/
+
